4cbf077fda093bc480025614fc3ab3eb947f7498
Kingsley Maina <mainakingsley7@gmail.com>
2023-10-04 11:51:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversed = word.split("").reverse().join("")
+  if(reversed === word){
+    return true
+  }
+  else{
+    return false
+  }
 }
-
-/* 
+ /*
   Add your pseudocode here
-*/
 
+reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false*/
+
+ 
 /*
   Add written explanation of your solution here
+
+  create a function that carry out the test
+  declare a variable which will hold the data of the reversed words
+  an if statement that returns true if the word Is similar if reversed and returns false if not
 */
 
 // You can run `node index.js` to view these console logs
